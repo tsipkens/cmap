efe7c7896ef20d04cb56f27576e16439e93cceeb
Timothy A. Sipkens <tsipkens@gmail.com>
2020-12-04 11:53:45 -0500

    Create submodule_add.sh
    This is a simple bash script that can be used to add the submodule to a git project.

---
+++ submodule_add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# SUBMODULE_ADD
+# A git routine for adding cmap as a submodule into a program.
+# Author:  Timothy Sipkens, 2020-09-17
+#===========================================================#
+
+git submodule add -b master https://github.com/tsipkens/cmap
+git submodule init
