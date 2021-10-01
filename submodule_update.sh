0cbe92c11b34cb55083c40ce4688942ba0420267
tsipkens <tsipkens@gmail.com>
2021-10-01 11:03:23 -0400

    Add submodule_update script
    This is a shell script that can be used to update the submodules of a project.

---
+++ submodule_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# SUBMODULE_UPDATE
+# Updates git submodules in current program.
+# Author:  Timothy Sipkens, 2019-07-15
+#===========================================================#
+
+git submodule update --recursive --remote
