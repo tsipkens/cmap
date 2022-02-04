3750edb5c3fe0de1a98bd3f554bf8a9fd58ae59a
tsipkens <tsipkens@gmail.com>
2022-02-04 01:31:01 -0500

    Add get_data and write_cmaps scripts

---
+++ +tools/get_data.m
@@ -0,0 +1,20 @@
+
+% GET_DATA  Get data from files and write to CSV files.
+% Author: Timothy Sipkens, 2019
+%================================================================%
+
+clear;
+
+fn = dir(fullfile('','*.m'));
+
+for ii=1:length(fn)
+    if ~strcmp(fn(ii).name, 'cmap_sweep.m') % skip test data file
+        % cmap_name = fn(ii).name(1:(end-4));
+        cmap_name = fn(ii).name(1:(end-2));
+        n = 350;
+        cm = eval([cmap_name, '(', num2str(n), ')']);
+        
+        dlmwrite(['data/',cmap_name,'.csv'], cm, 'delimiter', ',', 'precision', 9);
+    end
+end
+
